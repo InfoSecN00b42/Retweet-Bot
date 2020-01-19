6bb3793215eceab5231e094cb9f448fccd086c9c
InfoSecN00b <InfoSecN00b42@protonmail.ch>
2020-01-20 07:54:32 +0900

    We are not a RT bot anymore, RIP

--- RetweetObject.py
+++ RetweetObject.py
@@ -1,9 +1,12 @@
 class RetweetObject:
     def __init__(self, tweet):
-        if(not hasattr(tweet, "retweeted_status")):
-            raise ValueError("Tweet needs to contain a retweeted status")
-        self.id = tweet.retweeted_status.id
-        self.authorId = tweet.retweeted_status.user.id
+        # We are NOT a plain retweet bot anymore and will not get a retweeted status from our quoted tweets.
+        ##if(not hasattr(tweet, "retweeted_status")):
+        ##    raise ValueError("Tweet needs to contain a retweeted status")
+        ##self.id = tweet.retweeted_status.id
+        ##self.authorId = tweet.retweeted_status.user.id
+        self.id = tweet.id
+        self.authorId = tweet.user.id
         self.tweet = tweet
 
     def hasUserId(self, userId):
