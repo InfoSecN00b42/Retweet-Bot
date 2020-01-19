3a559872d91ffe8aab57acaac26fe34701bb900c
InfoSecN00b <InfoSecN00b42@protonmail.ch>
2020-01-20 08:10:40 +0900

    The pattern file

---
+++ patterns.py
@@ -0,0 +1,31 @@
+substitutions = \
+        [ "(?i)infosec:InfoSex", \
+            "(?i)secop?:SexOps", \
+            "(?i)#potato:🥔", \
+            "(?i)potato:🥔", \
+            "(?i)cybersec:potatosex", \
+            "(?i)cyber:potato", \
+            "(?i)critical:🦞", \
+            "(?i)Microsoft:$MSFT", \
+            "(?i)threat:hype", \
+            "(?i)ransomware:handsomeware", \
+            "(?i)bigdata:bigblabla", \
+            "(?i)2FA:0fuckAuth", \
+            "(?i)security:insecurity", \
+            "(?i)hacker?:🐱💻", \
+            "(?i) and : & ", \
+            "(?i)iot:IoT💩", \
+            "(?i)pray:🛐", \
+            "(?i)&gt;:➡️", \
+            "(?i)&amp;:&", \
+            "(?i)GCHQ:👻", \
+            "CIA:👻", \
+            "FBI:👻", \
+            "NSA:👻", \
+            "U.S.:🇺🇲", \
+            "(?i)Iran:🇮🇷", \
+            "(?i)Japan:🇯🇵", \
+            "(?i)Luxembourg:🇱🇺", \
+            "(?i)Luxemburg:🇱🇺", \
+            "(?i)Russia:🇷🇺", \
+                ]
