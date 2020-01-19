57bbc9c2aff1cf2e6bf2e058c915f5bcb99317c4
InfoSecN00b <InfoSecN00b42@protonmail.ch>
2020-01-20 07:43:59 +0900

    Fixed small bug

--- RetweetGuard.py
+++ RetweetGuard.py
@@ -3,12 +3,23 @@ from settings import minimum_account_age
 from settings import minimum_account_follower
 from time import sleep
 
+# The below is a bug from __init__ see commented line
+# [22404415, 13862172]
+#<class 'list'>
+#Traceback (most recent call last):
+#  File "retweet.py", line 31, in <module>
+#    retweetGuard = RetweetGuard(api.blocks_ids())
+#  File "/home/steve/mCode/Retweet-Bot/RetweetGuard.py", line 11, in __init__
+#    self._blocked_ids = blocked_ids['ids']
+#TypeError: list indices must be integers or slices, not str
+
 
 class RetweetGuard:
     _blocked_ids = []
 
     def __init__(self, blocked_ids):
-        self._blocked_ids = blocked_ids['ids']
+        #self._blocked_ids = blocked_ids['ids']
+        self._blocked_ids = blocked_ids
 
     def preliminary_tweet_test(self, tweet):
         # check if user blocked
@@ -34,7 +45,7 @@ class RetweetGuard:
         for retweet in tweetStore.getAllRetweets():
             if retweet.getUserId() in self._blocked_ids:
                 # Remove a retweet from timeline and store
-                # print("Trying to unretweet from " + retweet.getTweet().retweeted_status.user.name)
+                print("Trying to unretweet from " + retweet.getTweet().retweeted_status.user.name)
                 api.unretweet(retweet.getTweetId())
                 ids.append(retweet.getTweetId())
                 sleep(0.5)
